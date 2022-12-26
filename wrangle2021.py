bafccbd1a664908f0bc8f2a6a397d33679522f85
Oma-san <omasanakperi@yahoo.com>
2022-12-26 11:18:59 +0100

    Trying to create a class that wrangles my data

---
+++ wrangle2021.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import pyreadstat as pyr
+df, meta  = pyr.read_sav('afrobarometer_nigeria_rd8_2021.sav')
+
+class Wrangler:
+    """Methods for wrangling data."""
+
+    def __init__(self, df=df, meta=meta):
+
+        """init
+
+        Parameters
+        ----------
+        df: The dataframe that is loaded
+        meta: Information about the data
+        """
+        self.df, self.meta  = df, meta 
+
+    def wrangle(self, columns, rename):
+        """Wrangles the dataframe to have get the required columns.
+
+        Parameters
+        ----------
+        columns : List
+            List of columns that need to be extracted from the DataFrame
+        rename: List
+            List of new names the columns would have
+
+        Returns
+        -------
+        DataFrame
+            Dataframe with extracted columns.
+        """
+        # Select columns and replace the code numbers in columns with actual names in metadata
+        columns = columns
+        df_new = self.df.copy()[columns]
+        for i in columns:
+            x = meta.variable_value_labels[i]
+            df_new[i].replace(x, inplace=True)
+
+        # Rename columns
+        df_new.columns = rename
+
+        # Return DataFrame
+        return df_new
